eb4ee81073cb79cdda8db41fb7cf65bb6bef5695
Anthony Ortega <anthony.ortega@enapsys.com>
2024-05-26 17:23:28 -0600

    First commit

---
+++ src/main/java/com/ada/persistenciahomework/PersistenciaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.ada.persistenciahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenciaHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ada/persistenciahomework/PersistenciaHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.persistenciahomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
